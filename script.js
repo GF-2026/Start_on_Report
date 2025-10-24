fbcaaf7ba5b0eabfcb6a5257b630b4bafe62f95a
MVM <luis.covarrubias.fv@gmail.com>
2025-10-24 00:16:40 -0600

    Update script.js

--- script.js
+++ script.js
@@ -295,7 +295,7 @@ document.getElementById('exportBtn').addEventListener('click', ()=>{
     const ws = XLSX.utils.json_to_sheet(records);
     const wb = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(wb, ws, 'Reportes');
-    XLSX.writeFile(wb, 'Registro_de_preventivos.xlsx');
+    XLSX.writeFile(wb, 'Startup_reports.xlsx');
 });
 
 // ======================
