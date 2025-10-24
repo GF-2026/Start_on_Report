5bb8df0662c7a126478be8c1fc1705a9875b294c
MVM <luis.covarrubias.fv@gmail.com>
2025-10-23 23:51:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function generateFolio(){
     const now = new Date();
     const y = now.getFullYear(), m = String(now.getMonth()+1).padStart(2,'0'), d = String(now.getDate()).padStart(2,'0');
     const h = String(now.getHours()).padStart(2,'0'), min = String(now.getMinutes()).padStart(2,'0');
-    return `Startup_Report-${company}-${y}${m}${d}-${h}${min}`;
+    return `Preventive_Report-${company}-${y}${m}${d}-${h}${min}`;
 }
 
 // ======================
